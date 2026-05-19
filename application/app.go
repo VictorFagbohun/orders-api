b621f70a3c76db8f327f15da1ce61f0107736c32
VictorFagbohun <ofagbohun62@gmail.com>
2026-05-18 20:39:40 -0500

    pre config

--- application/app.go
+++ application/app.go
@@ -3,30 +3,36 @@ package application
 import (
 	"context"
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func New() *App {
 	app := &App{
-		router: loadRoutes(),
+		rdb: redis.NewClient(&redis.Options{Addr: "localhost:6379"}),
 	}
+
+	app.loadRoutes()
 
 	return app
 }
 
 func (a *App) Start(ctx context.Context) error {
 	server := &http.Server{
-		Addr: ":3000",
+		Addr:    ":3000",
 		Handler: a.router,
 	}
 
-	err := server.ListenAndServe()
-	if err != nil {
-		return fmt.Errorf("failed to start serverLL %w", err)
+	log.Printf("listening on %s", server.Addr)
+	if err := server.ListenAndServe(); err != nil {
+		return fmt.Errorf("failed to start server: %w", err)
 	}
 
 	return nil
